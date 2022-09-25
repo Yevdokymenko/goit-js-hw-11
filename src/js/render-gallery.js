66064a98d7c76c2784670ccf22098b7c280f875f
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-09-25 19:41:28 +0300

    done

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -20,10 +20,10 @@ function renderGallery(images) {
           <div class="gallery-item" id="${id}">
             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
+              <p class="info-item"><b>Лайків</b>${likes}</p>
+              <p class="info-item"><b>Глядачів</b>${views}</p>
+              <p class="info-item"><b>Коментів</b>${comments}</p>
+              <p class="info-item"><b>Завант-нь</b>${downloads}</p>
             </div>
           </div>
         </a>
