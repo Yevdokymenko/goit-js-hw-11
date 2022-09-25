68e17087e563049398f4c9bdf8f00467bf0c386f
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-09-25 18:30:46 +0300

    done hw

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import './sass/main.scss';
+import { fetchImages } from './js/fetch-images';
+import { renderGallery } from './js/render-gallery';
+import { onScroll, onToTopBtn } from './js/scroll';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.btn-load-more');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+onScroll();
+onToTopBtn();
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
+
+  if (query === '') {
+    alertNoEmptySearch();
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoImagesFound();
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        alertImagesFound(data);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+        alertEndOfSearch();
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function alertImagesFound(data) {
+  Notiflix.Notify.success(`УРА! Ми знайшли ${data.totalHits} зображень.`);
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure(
+    'Це поле не може бути порожнім. Будь ласка, визначтесь з пошуком.'
+  );
+}
+
+function alertNoImagesFound() {
+  Notiflix.Notify.failure(
+    'Вибачте, але по Вашому запиту не були знайдені зображення. Спробуйте ще раз.'
+  );
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.failure(
+    'Вибачте, але Ви досягли останнього зображення в результатах. Сподіваємось Вам сподобалось).'
+  );
+}
